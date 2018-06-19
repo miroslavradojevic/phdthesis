f6eca54b8855a169eb986579116af1d6fc71bd67
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2018-06-19 11:17:30 +0200

    Clean auxiliary files script finalized.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Clean auxiliary files..."
 
-# example of using arguments to a script
-echo "Cleaning files..."
-# clean all files with following extensions
+extensions='aux log bbl blg glo gz ist toc' # array with the extensions
+
+for ext in $extensions # go through the extensions
+do
+	echo "- "$ext
+	# clean all files with given extensions
+	find . -type f -iname "*.${ext}" -exec sh -c 'echo $0; rm -rf $0;' {} \;
+done
