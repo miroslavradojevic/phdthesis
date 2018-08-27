9005bef858481f0f922eea07f83a30bbf1365306
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2018-08-27 08:39:09 +0200

    Added auxilliary file cleanup to the unix build script.

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 sh clean.sh;
+rm -rf ./phdthesis.pdf
 pdflatex phdthesis;
 bibtex phdthesis;
 pdflatex phdthesis;
 makeglossaries phdthesis;
 pdflatex phdthesis;
-
+sh clean.sh;
+# one-line command
 # sh clean.sh; pdflatex phdthesis; bibtex phdthesis; pdflatex phdthesis; makeglossaries phdthesis; pdflatex phdthesis
