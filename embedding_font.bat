a82cb392aba60772e4629ca5fd824db32b044dea
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2018-12-12 21:12:42 +0100

    Updated script for emdedding font. Added alternative figures with embedded font.

--- embedding_font.bat
+++ embedding_font.bat
@@ -3,19 +3,21 @@
 echo Embedding fonts...
 
 SET FIG_EXT=.pdf
-SET FIG_SUFFIX=_embeddedf
+SET FIG_SUFFIX=_embedded
 echo:
-if not exist "out" mkdir out
-
-echo %cd%
 
 for %%s in (
 chapter2\L_fuzzification_1 
 chapter2\L_fuzzification_2 
 chapter2\U_fuzzification_1 
-chapter2\U_fuzzification_2) do (
-	echo %%s%FIG_EXT%
-	echo \out\%%s%FIG_SUFFIX%%FIG_EXT%
-	gswin64c -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -sOutputFile=\out\%%s%FIG_SUFFIX%%FIG_EXT% -f %%s%FIG_EXT%
+chapter2\U_fuzzification_2
+chapter2\C_fuzzification_1
+chapter2\C_fuzzification_2
+) do (
 	echo:
+	echo %%s
+	echo %%s%FIG_EXT%
+	echo %%s%FIG_SUFFIX%%FIG_EXT%
+	echo:
+	gswin64c -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -sOutputFile=%%s%FIG_SUFFIX%%FIG_EXT% -f %%s%FIG_EXT%
 )
