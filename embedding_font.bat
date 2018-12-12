a4a2f3af916c6f0256bed43a767c99af97891eb5
Miroslav Radojevic <miroslav.radojevic@gmail.com>
2018-12-12 23:02:10 +0100

    Updated font embedding script.

--- embedding_font.bat
+++ embedding_font.bat
@@ -13,6 +13,16 @@ chapter2\U_fuzzification_1
 chapter2\U_fuzzification_2
 chapter2\C_fuzzification_1
 chapter2\C_fuzzification_2
+chapter2\fig9a
+chapter2\fig9b
+chapter2\fig10a
+chapter2\fig10b
+chapter2\triplets_fjun_fend_vs_pratio
+chapter2\triplets_fboth_vs_snr
+chapter2\overview_real
+chapter2\compareJUN_all
+chapter2\compareEND_all
+chapter2\compareBOTH_all
 ) do (
 	echo:
 	echo %%s
